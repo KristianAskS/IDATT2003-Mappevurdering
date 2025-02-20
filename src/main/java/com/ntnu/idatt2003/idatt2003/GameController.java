8d0081efa31b8083f9fa9825551b748a43c66bbe
Tri <tritac.le@gmail.com>
2025-02-20 21:02:34 +0100

    Made base model classes and fxml file for GUI

--- src/main/java/com/ntnu/idatt2003/idatt2003/GameController.java
+++ src/main/java/com/ntnu/idatt2003/idatt2003/GameController.java
@@ -3,7 +3,7 @@ package com.ntnu.idatt2003.idatt2003;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class GameController {
   @FXML
   private Label welcomeText;
   
---
+++ src/main/java/com/ntnu/idatt2003/idatt2003/model/Board.java
@@ -0,0 +1,4 @@
+package com.ntnu.idatt2003.idatt2003.model;
+
+public class Board {
+}
