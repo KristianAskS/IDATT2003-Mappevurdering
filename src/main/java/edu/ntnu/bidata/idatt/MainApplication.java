c3523d9a813c0effd7c40bd9411c5171371dab5a
Tri <tritac.le@gmail.com>
2025-03-28 02:25:58 +0100

    Implemented a class (BoardGameInterface) that seperates the lifecycle management and the UI interactions

--- src/main/java/edu/ntnu/bidata/idatt/MainApplication.java
+++ src/main/java/edu/ntnu/bidata/idatt/MainApplication.java
@@ -1,6 +1,5 @@
 package edu.ntnu.bidata.idatt;
 
-import edu.ntnu.bidata.idatt.view.SceneManager;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -13,10 +12,8 @@ public class MainApplication extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    new SceneManager(primaryStage);
-    ;
-    SceneManager.showLandingScene();
-    primaryStage.setTitle("Board Games");
-    primaryStage.show();
+    BoardGameInterface boardGameInterface = new BoardGameInterface(primaryStage);
+    boardGameInterface.init();
+    boardGameInterface.start();
   }
 }
