cb648e01fee7b688ff91ce51f2a34ca8100895ab
Tri Tac Le <tritac.le@gmail.com>
2025-04-10 21:50:47 +0200

    Reformatted the code to adhere to google checkstyle rules

--- src/main/java/edu/ntnu/bidata/idatt/view/components/SnakesAndLaddersView.java
+++ src/main/java/edu/ntnu/bidata/idatt/view/components/SnakesAndLaddersView.java
@@ -8,11 +8,8 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
