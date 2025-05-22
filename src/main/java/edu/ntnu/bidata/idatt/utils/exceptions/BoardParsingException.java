8c11f2933531c05bf52ab79a161a4a56d1545e57
Tri Tac Le <tritac.le@gmail.com>
2025-05-22 20:14:14 +0200

    feat(): Wrote javadocs for exceptions classes

--- src/main/java/edu/ntnu/bidata/idatt/utils/exceptions/BoardParsingException.java
+++ src/main/java/edu/ntnu/bidata/idatt/utils/exceptions/BoardParsingException.java
@@ -1,12 +1,23 @@
 package edu.ntnu.bidata.idatt.utils.exceptions;
 
 import com.google.gson.JsonSyntaxException;
-
+import edu.ntnu.bidata.idatt.model.entity.Board;
 /**
- * A domain spesific exception shows error when parsing a board from a JSON file
+ * <p>Indicates that an error occurred while parsing a {@link Board}
+ * from a JSON file.</p>
+ * <p>Wraps the underlying {@link JsonSyntaxException} and includes the file path.</p>
+ *
+ * @author Tri Tac Le
+ * @since 1.0
  */
 public class BoardParsingException extends RuntimeException {
 
+  /**
+   * Constructs a new BoardParsingException.
+   *
+   * @param filePath the path of the JSON file that failed to parse
+   * @param error    the underlying {@link JsonSyntaxException} cause
+   */
   public BoardParsingException(String filePath, JsonSyntaxException error) {
     super("Error parsing board from: " + filePath, error);
   }
--- src/main/java/edu/ntnu/bidata/idatt/utils/exceptions/GameUIException.java
+++ src/main/java/edu/ntnu/bidata/idatt/utils/exceptions/GameUIException.java
@@ -1,11 +1,29 @@
 package edu.ntnu.bidata.idatt.utils.exceptions;
 
+/**
+ * <p>Indicates an error occurring within the game’s ui layer.</p>
+ *
+ * @author Kristian Ask Selmer
+ * @since 1.0
+ */
 public class GameUIException extends RuntimeException {
 
+  /**
+   * Constructs a new GameUIException with the message and cause.
+   *
+   * @param message a description of the UI error
+   * @param cause   the exception that triggered this UI failure
+   */
   public GameUIException(String message, Throwable cause) {
     super(message, cause);
   }
 
+  /**
+   * Constructs a new GameUIException with a message and the cause.
+   *
+   * @param cause the exception that triggered this UI failure
+   */
+  @SuppressWarnings("Unused")
   public GameUIException(Throwable cause) {
     super("Unexpected UI error", cause);
   }
