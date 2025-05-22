c262d1b029f5f12c7c43845ba2a046ed233d8a00
KristianAskS <kristians.sin.konto@gmail.com>
2025-05-22 18:40:19 +0200

    refactor(Ladder/Snake): seperated concerns

--- src/main/java/edu/ntnu/bidata/idatt/view/components/SnakeView.java
+++ src/main/java/edu/ntnu/bidata/idatt/view/components/SnakeView.java
@@ -2,9 +2,6 @@ package edu.ntnu.bidata.idatt.view.components;
 
 import edu.ntnu.bidata.idatt.controller.GameController;
 import edu.ntnu.bidata.idatt.model.entity.Board;
-import edu.ntnu.bidata.idatt.model.entity.Snake;
-import edu.ntnu.bidata.idatt.model.logic.action.SnakeAction;
-import edu.ntnu.bidata.idatt.utils.exceptions.GameUIException;
 import java.util.logging.Logger;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -17,43 +14,11 @@ public final class SnakeView {
   private SnakeView() {
   }
 
-  public static void drawSnakes(Board board, GridPane boardGrid, Pane overlay,
-                                GameController gameController) {
-
-    board.getTiles().values().stream()
-        .filter(tile -> tile.getLandAction() instanceof SnakeAction)
-        .forEach(start -> {
-          SnakeAction snakeAction = (SnakeAction) start.getLandAction();
-          int startId = start.getTileId();
-          int endId = snakeAction.getDestinationTileId();
-          if (isValidSnake(startId, endId)) {
-            return;
-          }
-
-          try {
-            overlay.getChildren().addAll(
-                new Snake(start, board.getTile(endId), board, boardGrid, gameController).getSnakes()
-            );
-          } catch (Exception exception) {
-            throw new GameUIException(
-                "Failed to draw snake from tile " + startId + " to tile " + endId, exception);
-          }
-
-          TileView startView = (TileView) boardGrid.lookup("#tile" + startId);
-          TileView endView = (TileView) boardGrid.lookup("#tile" + endId);
-          if (startView != null) {
-            startView.setStyle("-fx-background-color:red;");
-          }
-          if (endView != null) {
-            endView.setStyle("-fx-background-color:#FF474D;");
-          }
-
-        });
+  public static void drawSnakes(Board board, GridPane boardGrid, Pane overlayPane,
+      GameController gameController) {
+    SnakeRenderer.drawSnakes(board, boardGrid, overlayPane, gameController);
   }
 
-  /**
-   * head must be above tail and not on the same horizontal row.
-   */
   public static boolean isValidSnake(int headId, int tailId) {
     if (headId <= tailId) {
       return true;
