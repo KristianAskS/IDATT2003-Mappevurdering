61e38a30dae468ce0c908f806bcae63b91788842
Tri Tac Le <tritac.le@gmail.com>
2025-05-23 09:38:45 +0200

    Converted snake class to a record class

--- src/main/java/edu/ntnu/bidata/idatt/model/entity/Snake.java
+++ src/main/java/edu/ntnu/bidata/idatt/model/entity/Snake.java
@@ -4,10 +4,7 @@ package edu.ntnu.bidata.idatt.model.entity;
 /**
  * Visual representation of a snake connecting two {@link Tile} instances on a Snakes and Ladders board.
  */
-public class Snake {
-
-  private final int startTileId;
-  private final int endTileId;
+public record Snake(int startTileId, int endTileId) {
 
   /**
    * Constructs a new snake between the specified start and end tile ids
@@ -15,17 +12,7 @@ public class Snake {
    * @param startTileId
    * @param endTileId
    */
-  public Snake(int startTileId, int endTileId) {
-    this.startTileId = startTileId;
-    this.endTileId = endTileId;
-  }
-
-  public int getStartTileId() {
-    return startTileId;
-  }
-
-  public int getEndTileId() {
-    return endTileId;
+  public Snake {
   }
 
 }
